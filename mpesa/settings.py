2ea14bdb3084fde32a257ac043dbb6875f698571
nelziositoe@gmail.com <nelziositoe@gmail.com>
2019-06-01 12:34:01 +0200

    add host

--- mpesa/settings.py
+++ mpesa/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['mpesa-fa.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['mpesa-fa.herokuapp.com', 'mpesa-wsdk.herokuapp.com' '127.0.0.1']
 
 
 # Application definition
